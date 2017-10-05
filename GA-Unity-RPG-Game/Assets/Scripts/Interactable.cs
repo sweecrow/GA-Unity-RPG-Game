6711ec627c5913cabf6789ed8193b881d30dfef2
sweecrow <william.alfonsson@hotmail.com>
2017-10-05 19:14:05 +0200

    movements and interaction
    I added movement to the player, when clicking on the ground the player move to that point.
    
    we have an interactible script that can be used to interacit with all objekt we set it to

---
+++ GA-Unity-RPG-Game/Assets/Scripts/Interactable.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact ()
+    {
+        //this metdhod is meatn to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    void Update()
+    {
+        if(isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }    
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused ()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    void OnDwawGizmosSelected ()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
