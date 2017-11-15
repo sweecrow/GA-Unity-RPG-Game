34a804b626f05a9033c34bd437c6a1f1fd487797
sweecrow <william.alfonsson@hotmail.com>
2017-11-15 16:45:06 +0100

    Equipment
    Added an Equipment system for armor and wapons.

---
+++ GA-Unity-RPG-Game/Assets/EquipmentManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EquipmentManager : MonoBehaviour {
+
+    public static EquipmentManager instance;
+
+    #region Singleton
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    Equipment[] currentEquipment;
+
+    public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
+    public OnEquipmentChanged onEquipmentChanged;
+
+    Inventory inventory;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+
+        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numSlots];
+    }
+
+    public void Equip (Equipment newItem)
+    {
+        int slotIndex = (int)newItem.equipSlot;
+
+        Equipment oldItem = null;
+
+        if (currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+        }
+
+        if (onEquipmentChanged != null)
+        {
+            onEquipmentChanged.Invoke(newItem, oldItem);
+        }
+
+        currentEquipment[slotIndex] = newItem;
+    }
+
+    public void Unequip (int slotIndex)
+    {
+        if (currentEquipment[slotIndex] != null)
+        {
+            Equipment oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+
+            currentEquipment[slotIndex] = null;
+
+            if (onEquipmentChanged != null)
+            {
+                onEquipmentChanged.Invoke(null, oldItem);
+            }
+        }
+    }
+
+    public void UnequiptAll ()
+    {
+        for (int i = 0; i < currentEquipment.Length; i++)
+        {
+            Unequip(i);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            UnequiptAll();
+        }
+    }
+}
---
+++ GA-Unity-RPG-Game/Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName ="Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot equipSlot;
+
+    public int armorModifier;
+    public int damageModifer;
+
+    public override void Use()
+    {
+        base.Use();
+        //equip the item
+        EquipmentManager.instance.Equip(this);
+        // Remove it from the inventory
+        RemoveFromInventory();
+    }
+}
+public enum EquipmentSlot { Head, Cheast, Legs, Weapon, Shield, Feet }
