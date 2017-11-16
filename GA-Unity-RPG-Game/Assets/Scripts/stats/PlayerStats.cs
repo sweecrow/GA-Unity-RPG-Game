dc7749aeb621284c47d5b9afe61a259a848cb12b
sweecrow <william.alfonsson@hotmail.com>
2017-11-16 17:25:12 +0100

    Stats
    Added stats scripts for player thats workes with armor modifiers.

---
+++ GA-Unity-RPG-Game/Assets/Scripts/stats/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerStats : CharactarStats {
+
+	// Use this for initialization
+	void Start ()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+	}
+
+	// Update is called once per frame
+	void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if(newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifer);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifer);
+        }
+
+
+    }
+}
