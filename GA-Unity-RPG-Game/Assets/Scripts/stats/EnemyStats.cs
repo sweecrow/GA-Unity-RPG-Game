d8ab5b471a03fcf8622402e66ff2fe27c27a5f7e
sweecrow <william.alfonsson@hotmail.com>
2017-11-27 17:00:28 +0100

    Smaller Changes

--- GA-Unity-RPG-Game/Assets/Scripts/stats/EnemyStats.cs
+++ GA-Unity-RPG-Game/Assets/Scripts/stats/EnemyStats.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class EnemyStats : CharactarStats {
 
+    Animator myAnimator;
+
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.enabled = true;
+    }
+
     public override void Die()
     {
         base.Die();
 
         //Add death animation
+        myAnimator.SetBool("IsDead", true);
         
         Destroy(gameObject);
     }
