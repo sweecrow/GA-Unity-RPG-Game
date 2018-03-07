da2d3ab0840664fe49a3c3d62174914db686fb7e
sweecrow <william.alfonsson@hotmail.com>
2018-03-07 10:58:22 +0100

    Enemy spawning
    I completed the spawning script. You can set an area were enemies can spawn. how many you want to spawn, and how often they should spawn.

---
+++ GA-Unity-RPG-Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] enemies;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randEnemy;
+    public int enemieCount;
+    public int enemieMaxCount;
+    
+	
+	void Start ()
+    {
+        StartCoroutine(WaitSpawner());
+        
+	}
+
+	
+	void Update ()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+
+        if(enemieCount >= enemieMaxCount)
+        {
+            stop = true;
+        }
+    }
+
+    IEnumerator WaitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+        
+        while(!stop)
+        {
+            randEnemy = Random.Range(0, 2);
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            enemieCount++;
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+        
+    }
+}
